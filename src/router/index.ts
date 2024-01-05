68c8acd5e80fc8d730f5033a3c4c2b0ad1ec5255
sophie-jhkim <creatorsophie@gmail.com>
2024-01-05 17:57:40 +0900

    Update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,112 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { h, nextTick, resolveComponent } from "vue";
 import type { RouteRecordRaw } from "vue-router";
 import BaseLayout from "../layouts/BaseLayout.vue";
 
 const routes = [
     {
-        path: "",
+        path: "/",
         component: BaseLayout,
         children: [
             {
                 path: "",
-                name: "home",
+                name: "Home",
                 component: () => import("../views/Index.vue"),
                 meta: { requireLogin: false },
             },
+            {
+                path: "gnb1",
+                name: "Gnb1",
+                component: {
+                    render() {
+                        return h(resolveComponent("router-view"));
+                    },
+                },
+                children:[
+                    {
+                        path: "",
+                        name: "Gnb1DashBoard",
+                        component: () => import("../views/Gnb/Gnb1Index.vue"),
+                    },
+                    {
+                        path: "sub1",
+                        name: "Gnb1Sub1",
+                        children:[
+                            {
+                                path:"",
+                                name:"Gnb1Sub1Index",
+                                component: () => import("../views/Gnb/Gnb1Sub1Index.vue"),
+                            },
+                            {
+                                path:":id",
+                                name:"Gnb1Sub1Detail",
+                                component: () => import("../views/Gnb/Gnb1Sub1Detail.vue"),
+                            },
+                        ]
+                    },
+                    {
+                        path: "sub2",
+                        name: "Gnb1Sub2",
+                        children:[
+                            {
+                                path:"",
+                                name:"Gnb1Sub2Index",
+                                component: () => import("../views/Gnb/Gnb1Sub2Index.vue"),
+                            },
+                            {
+                                path:":id",
+                                name:"Gnb1Sub2Detail",
+                                component: () => import("../views/Gnb/Gnb1Sub2Detail.vue"),
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                path: "gnb2",
+                name: "Gnb2",
+                redirect: "/gnb2/sub1",
+                component: {
+                    render() {
+                        return h(resolveComponent("router-view"));
+                    },
+                },
+                children:[
+                    {
+                        path: "sub1",
+                        name: "Gnb2Sub1",
+                        children:[
+                            {
+                                path:"",
+                                name:"Gnb2Sub1Index",
+                                component: () => import("../views/Gnb/Gnb2Sub1Index.vue"),
+                            },
+                            {
+                                path:":id",
+                                name:"Gnb2Sub1Detail",
+                                component: () => import("../views/Gnb/Gnb2Sub1Detail.vue"),
+                            },
+                        ]
+                    },
+                    {
+                        path: "sub2",
+                        name: "Gnb2Sub2",
+                        redirect: "/gnb2/sub2/all",
+                        children:[
+                            {
+                                path:":type(all|apple|banana|orange)",
+                                name:"Gnb2Sub2Dynamic",
+                                component: () => import("../views/Gnb/Gnb2Sub2Index.vue"),
+                            },
+                        ]
+                    },
+                ]
+            },
+
+
             // menu1
             {
-                path: "/menu1",
+                path: "menu1",
                 name: "menu1",
                 /*omponent: BaseLayout,*/
                 children: [
@@ -35,7 +126,7 @@ const routes = [
             },
             // menu2
             {
-                path: "/menu2",
+                path: "menu2",
                 name: "menu2",
                 /*component: BaseLayout,*/
                 children: [
